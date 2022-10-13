5d1431e33d7e479274b49ea4e78968e7e04cd92c
ezzyadri <114646040+ezzyadri@users.noreply.github.com>
2022-10-13 09:59:10 +0200

    Add files via upload

---
+++ PruebaDepuracion.java
@@ -0,0 +1,8 @@
+public class PruebaDepuracion {
+	public static void main(String[] args) {
+		int anyo_actual=2022; 
+		int anyo_nacimiento=1990;
+		int edad=anyo_actual -anyo_nacimiento;
+		System.out.println( "Hola, tienes "+edad+" años");
+	}
+}
